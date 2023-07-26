eacd75a2fe176ade7fe08cc8a3bfb20697979b45
Chad Kimes <ckimes@gmail.com>
2023-07-26 14:12:20 -0400

    Forcibly exit node once the main promise finishes

--- src/restore.ts
+++ src/restore.ts
@@ -2,7 +2,14 @@ import restoreImpl from "./restoreImpl";
 import { StateProvider } from "./stateProvider";
 
 async function run(): Promise<void> {
-    await restoreImpl(new StateProvider());
+    try {
+        await restoreImpl(new StateProvider());
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
+
+    process.exit(0)
 }
 
 run();
